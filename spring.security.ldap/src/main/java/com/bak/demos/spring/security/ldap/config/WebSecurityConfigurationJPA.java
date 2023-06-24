c6e377268ca5e9ae0d3068a6b845ac7c321dd0ac
anilkumarb1248 <anilkumarb1248@gmail.com>
2023-06-24 23:29:33 +0530

    Spring security with actual LDAP Server

--- spring.security.ldap/src/main/java/com/bak/demos/spring/security/ldap/config/WebSecurityConfigurationJPA.java
+++ spring.security.ldap/src/main/java/com/bak/demos/spring/security/ldap/config/WebSecurityConfigurationJPA.java
@@ -1,5 +1,6 @@
 package com.bak.demos.spring.security.ldap.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,18 +11,49 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfigurationJPA extends WebSecurityConfigurerAdapter {
 
+    @Value("${ldap.urls}")
+    private String ldapUrls;
+
+    @Value("${ldap.base.dn}")
+    private String ldapBaseDn;
+
+    @Value("${ldap.username}")
+    private String ldapSecurityPrincipal;
+
+    @Value("${ldap.password}")
+    private String ldapPrincipalPassword;
+
+    @Value("${ldap.user.dn.pattern}")
+    private String ldapUserDnPattern;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        /**
+         * For Embedded LDAP Server
+         */
+//        auth
+//                .ldapAuthentication()
+//                .userDnPatterns("uid={0},ou=people")
+//                .groupSearchBase("ou=groups")
+//                .contextSource()
+//                .url("ldap://localhost:8389/dc=springframework,dc=org")
+//                .and()
+//                .passwordCompare()
+//                .passwordEncoder(new BCryptPasswordEncoder())
+//                .passwordAttribute("userPassword");
+
+        /**
+         * For Actual LDAP Server
+         */
         auth
                 .ldapAuthentication()
-                .userDnPatterns("uid={0},ou=people")
-                .groupSearchBase("ou=groups")
                 .contextSource()
-                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .url(ldapUrls + ldapBaseDn)
+                .managerDn(ldapSecurityPrincipal)
+                .managerPassword(ldapPrincipalPassword)
                 .and()
-                .passwordCompare()
-                .passwordEncoder(new BCryptPasswordEncoder())
-                .passwordAttribute("userPassword");
+                .userDnPatterns(ldapUserDnPattern);
     }
 
     @Override
